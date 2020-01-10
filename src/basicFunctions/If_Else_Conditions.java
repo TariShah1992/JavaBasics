ae75bf9262660fc5e852b07053bb75b3a2c6d231
unknown <tarishah1992@gmail.com>
2020-01-09 21:54:36 -0500

    first javaBasic

---
+++ src/basicFunctions/If_Else_Conditions.java
@@ -0,0 +1,35 @@
+package basicFunctions;
+
+public class If_Else_Conditions {
+
+	public static void main(String[] args) {
+		
+		int a = 800;
+		int b = 600;
+		int c = 900;
+				
+		if(a >= b & a>=c){
+			
+			System.out.println("a is greatest number");} 
+		
+		else if (b >=a & b>=c){
+			
+			System.out.println("b is the greatest number");}
+			
+			else{ System.out.println("C is the greates number");
+		
+		
+			 
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/basicFunctions/Loops_for_while.java
@@ -0,0 +1,44 @@
+package basicFunctions;
+
+public class Loops_for_while {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=20; i++){
+			
+			System.out.println(i);
+			
+		}
+		
+		System.out.println("***************************************");
+		
+		int i=1;
+		while(i<=10){
+		System.out.println(i);
+		i =i+1;
+				
+		}
+		
+		int a =10;
+		int b =20;
+		int c =30;
+		
+		if(a<b & a>c){
+			
+			System.out.println("a is the smallest number");
+		}
+		
+		else if (b>a & b>c){
+			System.out.println("b is a great number");
+		} else
+		{ System.out.println("c is the biggest number");
+		}
+		
+		
+		
+		 }
+		
+
+	
+
+}
